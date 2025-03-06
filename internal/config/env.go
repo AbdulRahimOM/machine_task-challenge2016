c9e8d5e4946fc009baff6cb4db007fb912e43ecb
Abdul Rahim O M <abdulrahimovingal@gmail.com>
2025-03-06 18:20:20 +0530

    Rate limiting (#13)

--- internal/config/env.go
+++ internal/config/env.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -13,7 +14,8 @@ const (
 )
 
 var (
-	Port string
+	Port      string
+	RateLimit int
 )
 
 func init() {
@@ -31,6 +33,15 @@ func loadEnv() {
 	Port = os.Getenv("PORT")
 	if Port == "" {
 		Port = defaultPort
+	}
+
+	RateLimit,err = strconv.Atoi(os.Getenv("RATE_LIMIT"))
+	if err != nil {
+		if os.Getenv("RATE_LIMIT") == "" {
+			RateLimit = 60
+		} else {
+			log.Fatal("Error loading RATE_LIMIT from .env file. err", err)
+		}
 	}
 
 }
